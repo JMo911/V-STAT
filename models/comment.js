d529fc80208c9e21a64b637a7280895200bc340e
Jacob Moss <jacobmoss09@gmail.com>
2019-10-31 18:35:27 -0500

    comment model added and associated to user as expected

---
+++ models/comment.js
@@ -0,0 +1,11 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const Comment = sequelize.define('Comment', {
+    message: DataTypes.STRING
+  }, {});
+  Comment.associate = function(models) {
+    // associations can be defined here
+    Comment.belongsTo(models.User)
+  };
+  return Comment;
+};
