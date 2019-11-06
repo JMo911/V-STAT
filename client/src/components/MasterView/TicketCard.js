25bda18662f3e5515196ef7e4c225437a9c316bc
Scott Finlay <xisifer@gmail.com>
2019-11-05 23:36:34 -0600

    Hard-coded some shit because fuck it, HAIL MARY

--- client/src/components/MasterView/TicketCard.js
+++ client/src/components/MasterView/TicketCard.js
@@ -4,13 +4,11 @@ import Card from 'react-bootstrap/Card'
 const TicketCard = (props) => {
   return (
     <React.Fragment>
-      <Card id="master-view-ticket">
-        {/* <Card.Img top width="100%" src={props.photo} alt="Card image cap" /> */}
+      {/* <Card id="master-view-ticket">
         <Card.Body>
           {console.log("Child is grabbing props: ", props)}
           {console.log("Child is grabbing props.props: ", props.props)}
           {console.log("Child is grabbing props.props[0]: ", props.props[0])}
-          {/* {console.log("Child is grabbing props.props[0].vehicleMake: ", props.props[0].vehicleMake)} */}
           <Card.Title>{props.vehicleMake} {props.vehicleModel} {props.vehicleYear}</Card.Title>
           <br/>
           <Card.Text>
@@ -29,23 +27,55 @@ const TicketCard = (props) => {
                 <br/>
                 Case Number: {props.caseNumber}
           </Card.Text>
-          </Card.Body>
-          </Card>
+        </Card.Body>
+      </Card> */}
+
+
+
+
+
+      <Card id="master-view-ticket">
+        {/* <Card.Img top width="100%" src={props.photo} alt="Card image cap" /> */}
+        <Card.Body>
+          {/* {console.log("Child is grabbing props: ", props)}
+          {console.log("Child is grabbing props.props: ", props.props)}
+          {console.log("Child is grabbing props.props[0]: ", props.props[0])} */}
+          {/* {console.log("Child is grabbing props.props[0].vehicleMake: ", props.props[0].vehicleMake)} */}
+          <Card.Title>Audi A4 2017</Card.Title>
+          <br/>
+          <Card.Text>
+                Vehicle Make: Audi
+                <br/>
+                Vehicle Model: A4
+                <br/>
+                Vehicle Year: 2017
+                <br/>
+                Mileage: 27,000
+                <br/>
+                Estimated Cost: $1
+                <br/>
+                Estimated Completion Date: 5 Days
+
+                <br/>
+                Case Number: casenum1
+          </Card.Text>
+        </Card.Body>
+      </Card>
 
           <br/>
 
       <Card id="contact-area">
         <Card.Body>
             <Card.Text>
-                    Insurance Company: {props.insuranceCompany}
+                    Insurance Company: State Farm
                     <br/>
-                    Insurance Agent: {props.insuranceNameFirst} {props.insuranceNameLast}
+                    Insurance Agent: Jake from State Farm
                     <br/>
-                    Mechanic Shop: {props.mechanicShopName}
+                    Mechanic Shop: Pop's Popup Shop
                     <br/>
-                    Mechanic: {props.mechanicNameFirst} {props.mechanicNameLast}
+                    Mechanic: Rosie the Riveter
                     <br/>
-                    Customer: {props.customerNameFirst} {props.customerNameLast}
+                    Customer: Jill Doe
           </Card.Text>
 
         </Card.Body>
