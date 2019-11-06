1a479a8cc69112faed29418469dcafa4985521d4
Jacob Moss <jacobmoss09@gmail.com>
2019-11-06 15:10:38 -0600

    dynamic contents render per ticket

--- client/src/components/Comments/Comment.js
+++ client/src/components/Comments/Comment.js
@@ -39,18 +39,70 @@ class Comments extends Component {
 
     handleSubmit(event) {
 
-        const comment = {
-            message: this.state.comment,
-            UsertypeId: 1
-        }
-        axios.post('/api/comments', comment)
-            .then(function (response) {
-            })
-            .catch(function (error) {
-                console.log(error);
-            }); 
+        // const comment = {
+        //     message: this.state.comment,
+        //     UsertypeId: 1
+        // }
+        // axios.post('/api/comments', comment)
+        //     .then(function (response) {
+        //     })
+        //     .catch(function (error) {
+        //         console.log(error);
+        //     }); 
         event.preventDefault();
     }
+
+    componentWillMount() {
+        let cookie = document.cookie;
+        cookie = cookie.split('; ');
+        let userId = cookie[0].split('=');
+        let finalUserId = userId[1];
+
+        // console.log("our real user ID is: " + finalUserId);
+
+        var result = {};
+        for (var i = 0; i < cookie.length; i++) {
+            var cur = cookie[i].split('=');
+            result[cur[0]] = cur[1];
+        }
+        let token = result.token;
+        let userCredentials = token.split('; ');
+        let finalToken = userCredentials[0];
+        const ticketNumber = window.location.href[window.location.href.length -1];
+        axios({
+            method: "get",
+            url: '/api/tickets/' + ticketNumber + "/comments",
+            headers: {
+              Authorization: "Bearer " + finalToken
+            }
+          })
+          .then(response => {
+            const data = response.data[0].Comments;
+            console.log("COMMENT DATA",data)
+            this.setState({comments: data})
+            const incompleteTasks=[];
+            const completeTasks=[];
+            // data.forEach(element => {
+            //     console.log(element.completed);
+            //     if(element.completed) {
+            //         completeTasks.push(element)
+            //         console.log(element)
+            //     } else {
+            //         incompleteTasks.push(element)
+            //     }
+            // });
+            // this.setState({
+            //     tasks:incompleteTasks,
+            //     completedTasks: completeTasks
+            // });
+            // console.log("TASK DATA",this.state.tasks)
+            // this.setState({ data:data })
+          }).catch(function(error) {
+            console.log("error:", error);
+          })
+          console.log(this.state.completedTasks)
+    }
+
 
     render() {
         return (
@@ -60,9 +112,9 @@ class Comments extends Component {
                     {this.state.comments.length ? (
                     <List>
                         {this.state.comments.map(comment => (
-                        <ListItem key={comment.commentId}>
-                                <img className="comment-avatar" src={comment.profilePic} alt="stuff" />
-                                {comment.author} : {comment.commentText}
+                        <ListItem key={comment.id}>
+                                {/* <img className="comment-avatar" src={comment.profilePic} alt="stuff" /> */}
+                                UserID: {comment.UserId}  Comment: {comment.message}
                                 {/* <DeleteBtn
                                     deleteBook = { () => this.deleteBook(book._id)} 
                                 /> */}
