143428d1ae8c3c7c6aec7923692b66f5fc70c149
Jacob Moss <jacobmoss09@gmail.com>
2019-11-06 18:12:34 -0600

    adding toms manifest code

---
+++ client/build/precache-manifest.5e856313bd45ec50e1f45df33fbc8ac6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "18e21c0bef55f1217dce",
+    "url": "/static/css/main.60fb5d51.chunk.css"
+  },
+  {
+    "revision": "18e21c0bef55f1217dce",
+    "url": "/static/js/main.3f7d1ede.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "e56a3c047ca7162c5ab8",
+    "url": "/static/js/2.0a4d6894.chunk.js"
+  },
+  {
+    "revision": "8816ceab2112354acc3cdb8609b648fb",
+    "url": "/index.html"
+  }
+];
