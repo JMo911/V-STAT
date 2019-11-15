1170a238bc2d3ac88c6545603a6a55920cf1ac2a
Jacob Moss <jacobmoss09@gmail.com>
2019-11-14 23:04:59 -0600

    revised path on the login index page

--- client/src/components/login/index.js
+++ client/src/components/login/index.js
@@ -3,6 +3,7 @@ import {
   Container
 } from "react-bootstrap";
 import './styles.css';
+// import '../../../public/assets/images/VSTAT-logo2.png'
 import { Link } from "react-router-dom";
 // import { Button } from "react-bootstrap";
 import posed from 'react-pose';
@@ -31,7 +32,8 @@ class LoginPage extends Component {
     return (
       <Container>
         <div id="loginArea">
-          <img src="/assets/images/VSTAT-logo2.png" id="logo" alt="V-STAT!" />
+        {/* <img src={require('/images/image-name.png')} /> */}
+          <img src={process.env.PUBLIC_URL + '/assets/images/VSTAT-logo2.png'} id="logo" alt="V-STAT!" />
           <p>Welcome to V-STAT!</p>
           <p>This program aims to take some of the confusion out of the car repair process, ensuring total transparency throughout every step of your journey. From the time a Customer gets into an accident, to the moment they pick up their fixed vehicle, V-STAT is there every step of the way. </p>
           <p>Whether you're an insurance agent, a mechanic, or a customer, V-STAT can help!</p>
