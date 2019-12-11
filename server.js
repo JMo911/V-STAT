b2d2e6cdbaca3b82004a284b399ee8cdd92c4a15
Jacob Moss <jacobmoss09@gmail.com>
2019-12-10 19:33:32 -0600

    replacing asterisk with slash in server.js

--- server.js
+++ server.js
@@ -39,7 +39,7 @@ if (process.env.NODE_ENV === "production") {
 
 // Send every request to the React app
 // Define any API routes before this runs
-app.get("*", function (req, res) {
+app.get("/", function (req, res) {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
